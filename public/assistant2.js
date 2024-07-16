b96c28831c039d9318d6fa599e109862e0530a9a
ypathan <ypathan@ypathans-MacBook-Air.local>
2024-07-16 17:12:30 +0800

    changed serverPort >> can login, logout >> can view first page, XXXXresource not loadingXXXX

--- public/assistant2.js
+++ public/assistant2.js
@@ -1,31 +1,44 @@
 var req = new XMLHttpRequest();
-req.open('GET',document.location,false);
+req.open("GET", document.location, false);
 // req.send(null);
-var link =window.location;
+var link = window.location;
 // var headers = req.getAllResponseHeaders().toLocaleLowerCase();
 // console.log('Header -- ',headers,' Link - ',link);
-if (link.type){
-  if (link.type==="DanaLocation"){
-    sessionStorage.setItem('serverPort',',DanaInfo=.adqBfwiFos1k2s,SSL+api/');
-    sessionStorage.setItem('photo',',DanaInfo=.adqBfwiFos1k2s,SSL+images/');
-    sessionStorage.setItem('accessChannel', 2)
+if (link.type) {
+  if (link.type === "DanaLocation") {
+    console.log("assistant line 9");
+    sessionStorage.setItem("serverPort", ",DanaInfo=.adqBfwiFos1k2s,SSL+api/");
+    // sessionStorage.setItem("serverPort", "http://localhost:8080/api/");
+    sessionStorage.setItem("photo", ",DanaInfo=.adqBfwiFos1k2s,SSL+images/");
+    // sessionStorage.setItem("photo", ",http://localhost:8080/images/");
+    sessionStorage.setItem("accessChannel", 2);
   }
-}else{
-  let url = link.origin+link.pathname+'api/';
-  let photo =link.origin+link.pathname+'images/';
-  sessionStorage.setItem('serverPort', url);
-  sessionStorage.setItem('photo',photo);
+} else {
+  console.log("assistant line 15");
+  let url = link.origin + link.pathname + "api/";
+  let photo = link.origin + link.pathname + "images/";
 
-  if(!sessionStorage.getItem('accessChannel')){
-    if ((link.host==="dsp.csd.hksarg") || (link.host ==="localhost") ){
-      sessionStorage.setItem('accessChannel', 1)
-    } else  if ((link.host==="dsptest.csd.ccgo.hksarg") || (link.host ==="localhost")){
-      sessionStorage.setItem('accessChannel', 1)
-    }      
-    else if (link.host==="kms.csd.gov.hk" || link.host==="kmst.csd.gov.hk"){
-      sessionStorage.setItem('accessChannel', 3)
-    }else{
-      sessionStorage.setItem('accessChannel', 4)
+  console.log("assistant line 19");
+  // sessionStorage.setItem('serverPort', url);
+  sessionStorage.setItem("serverPort", "http://localhost:8080/");
+  // sessionStorage.setItem("photo", photo);
+  sessionStorage.setItem("photo", "http://localhost:8080/");
+
+  if (!sessionStorage.getItem("accessChannel")) {
+    if (link.host === "dsp.csd.hksarg" || link.host === "localhost") {
+      sessionStorage.setItem("accessChannel", 1);
+    } else if (
+      link.host === "dsptest.csd.ccgo.hksarg" ||
+      link.host === "localhost"
+    ) {
+      sessionStorage.setItem("accessChannel", 1);
+    } else if (
+      link.host === "kms.csd.gov.hk" ||
+      link.host === "kmst.csd.gov.hk"
+    ) {
+      sessionStorage.setItem("accessChannel", 3);
+    } else {
+      sessionStorage.setItem("accessChannel", 4);
     }
   }
 }
--- src/service/AuthenticationService.js
+++ src/service/AuthenticationService.js
@@ -1,165 +1,175 @@
-import axios from 'axios'
+import axios from "axios";
 
 //  const API_URL = 'http://localhost:8080/'
 // const API_URL = sessionStorage.getItem('serverPort');
 
-export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
-export const SESSION_ATTRIBUTE_NAME_TOKEN = 'accessToken'
-export const SESSION_ATTRIBUTE_NAME_CHANNEL = 'accessChannel'
-export const SESSION_ATTRIBUTE_NAME_CODE = 'accessCode'
-export const CHANNEL_DP = 1
-export const CHANNEL_ACCESS_CSD_GOV_HK = 2
-export const CHANNEL_KMS_CSD_GOV_HK = 3
-export const CHANNEL_MOBILE_APP = 4
+export const USER_NAME_SESSION_ATTRIBUTE_NAME = "authenticatedUser";
+export const SESSION_ATTRIBUTE_NAME_TOKEN = "accessToken";
+export const SESSION_ATTRIBUTE_NAME_CHANNEL = "accessChannel";
+export const SESSION_ATTRIBUTE_NAME_CODE = "accessCode";
+export const CHANNEL_DP = 1;
+export const CHANNEL_ACCESS_CSD_GOV_HK = 2;
+export const CHANNEL_KMS_CSD_GOV_HK = 3;
+export const CHANNEL_MOBILE_APP = 4;
 
 class AuthenticationService {
+  //   API_URL = "http://localhost:8080";
+  //   executeBasicAuthenticationService(username, password) {
+  //     return axios.get(`${API_URL}/basicauth`, {
+  //       headers: { authorization: this.createBasicAuthToken(username, password) },
+  //     });
+  //   }
 
-    APP_URL= "http://localhost:8080"
-     executeBasicAuthenticationService(username, password) {
-         return axios.get(`${API_URL}/basicauth`,
-             { headers: { authorization: this.createBasicAuthToken(username, password) } })
-     }
-   
+  executeJwtAuthenticationService(
+    username,
+    password,
+    accessChannel,
+    accessCode
+  ) {
+    console.log(username, password, accessChannel, accessCode);
+    console.log("port: ", sessionStorage.getItem("serverPort"));
 
-    executeJwtAuthenticationService(username, password, accessChannel, accessCode) {
-         
+    //return axios.post(`${sessionStorage.getItem('serverPort')}auth/generatetoken`, {
+    //    username,
+    //    password,
+    //    accessChannel,
+    //    accessCode
+    //})
 
-        console.log(username, password, accessChannel, accessCode);
-        console.log("port: ", sessionStorage.getItem("serverPort"))
-    
-        //return axios.post(`${sessionStorage.getItem('serverPort')}auth/generatetoken`, {
-        //    username,
-        //    password,
-        //    accessChannel,
-        //    accessCode
-        //})
+    const url = "http://localhost:8080/";
+    return axios.post(`${url}auth/generatetoken`, {
+      username,
+      password,
+      accessChannel,
+      accessCode,
+    });
+    // const res = axios.post(`${sessionStorage.getItem('serverPort')}auth/generatetoken`, {
+    //         username,
+    //         password,
+    //         accessChannel,
+    //         accessCode
+    //     })
+    // console.log(res)
+  }
 
-        const url = 'http://localhost:8080/'
-            return axios.post(`${url}auth/generatetoken`, {
-            username,
-            password,
-            accessChannel,
-            accessCode
+  executeJwtAuthenticationServiceForDP(clientHost) {
+    return axios.post(
+      `${sessionStorage.getItem("serverPort")}auth/generatetokendp`,
+      {
+        clientHost,
+      }
+    );
+  }
+
+  createBasicAuthToken(username, password) {
+    return "Basic " + window.btoa(username + ":" + password);
+  }
+
+  registerSuccessfulLogin(username, password, accessChannel) {
+    sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
+    sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_CHANNEL, accessChannel);
+    this.setupAxiosInterceptors(this.createBasicAuthToken(username, password));
+  }
+
+  registerSuccessfulLoginForJwt(username, token, accessChannel) {
+    sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
+    sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_TOKEN, token);
+    sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_CHANNEL, accessChannel);
+    this.setupAxiosInterceptors(this.createJWTToken(token));
+  }
+
+  registerSuccessfulLoginForJwt1(token, accessChannel) {
+    sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_TOKEN, token);
+    sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_CHANNEL, accessChannel);
+    this.setupAxiosInterceptors(this.createJWTToken(token));
+  }
+
+  registerSuccessfulLoginForJwt2(username) {
+    sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
+  }
+
+  createJWTToken(token) {
+    return "Bearer " + token;
+  }
+
+  logout() {
+    sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
+  }
+
+  isUserLoggedIn() {
+    console.log("Is user new dp logged in ");
+    let hostname = window.location.hostname;
+    let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
+    if (hostname === "dsp.csd.hksarg" && user === null) {
+      //------for intranet
+      this.executeJwtAuthenticationServiceForDP(hostname)
+        .then((response) => {
+          this.registerSuccessfulLoginForJwt(
+            response.data.username,
+            response.data.accessToken,
+            CHANNEL_DP
+          );
+          console.log("Registered Success");
+
+          if (sessionStorage.getItem("_target")) {
+            window.location.assign(sessionStorage.getItem("_target"));
+            sessionStorage.removeItem("_target");
+          } else {
+            window.location.assign("#/home");
+          }
+          return true;
         })
-        // const res = axios.post(`${sessionStorage.getItem('serverPort')}auth/generatetoken`, {
-        //         username,
-        //         password,
-        //         accessChannel,
-        //         accessCode
-        //     })
-            // console.log(res)
-        
-    }
+        .catch(() => {
+          window.location.assign("#/");
+          console.log("Registered fail");
+          return false;
+        });
+    } else if (hostname === "dsptest.csd.ccgo.hksarg" && user === null) {
+      //------for intranet
+      this.executeJwtAuthenticationServiceForDP(hostname)
+        .then((response) => {
+          this.registerSuccessfulLoginForJwt(
+            response.data.username,
+            response.data.accessToken,
+            CHANNEL_DP
+          );
+          console.log("Registered Success");
 
-    executeJwtAuthenticationServiceForDP(clientHost) {
-        return axios.post(`${sessionStorage.getItem('serverPort')}auth/generatetokendp`, {
-            clientHost
+          if (sessionStorage.getItem("_target")) {
+            window.location.assign(sessionStorage.getItem("_target"));
+            sessionStorage.removeItem("_target");
+          } else {
+            window.location.assign("#/home");
+          }
+          return true;
         })
+        .catch(() => {
+          window.location.assign("#/");
+          console.log("Registered fail");
+          return false;
+        });
+    } else if (user === null) {
+      //------for remote access login
+      return false;
+    } else {
+      return true;
     }
+  }
 
-    createBasicAuthToken(username, password) {
-        return 'Basic ' + window.btoa(username + ":" + password);
-    }
+  getLoggedInUserName() {
+    let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
+    if (user === null) return "";
+    return user;
+  }
 
-    registerSuccessfulLogin(username, password, accessChannel) {
-        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username)
-        sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_CHANNEL, accessChannel)
-        this.setupAxiosInterceptors(this.createBasicAuthToken(username, password))
-    }
-
-    registerSuccessfulLoginForJwt(username, token, accessChannel) {
-        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username)
-        sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_TOKEN, token)
-        sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_CHANNEL, accessChannel)
-        this.setupAxiosInterceptors(this.createJWTToken(token))
-    }
-
-    registerSuccessfulLoginForJwt1(token, accessChannel) {
-        sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_TOKEN, token)
-        sessionStorage.setItem(SESSION_ATTRIBUTE_NAME_CHANNEL, accessChannel)
-        this.setupAxiosInterceptors(this.createJWTToken(token))
-    }
-
-    registerSuccessfulLoginForJwt2(username) {
-        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username)
-    }
-
-    createJWTToken(token) {
-        return 'Bearer ' + token
-    }
-
-    logout() {
-        sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
-    }
-
-    isUserLoggedIn() {
-        console.log('Is user new dp logged in ');
-        let hostname = window.location.hostname;
-        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
-        if (hostname === 'dsp.csd.hksarg' && user === null) { //------for intranet
-            this
-            .executeJwtAuthenticationServiceForDP(hostname)
-            .then((response) => {
-                this.registerSuccessfulLoginForJwt(response.data.username, response.data.accessToken, CHANNEL_DP)
-                console.log("Registered Success");
-                
-                if(sessionStorage.getItem('_target')){
-                    window.location.assign(sessionStorage.getItem('_target'));
-                    sessionStorage.removeItem('_target');
-                }else{
-                    window.location.assign('#/home');
-                }
-                return true;
-            }).catch(() => {
-                window.location.assign('#/');
-                console.log("Registered fail");
-                return false;
-            })
-        } else if (hostname === 'dsptest.csd.ccgo.hksarg' && user === null) { //------for intranet
-            this
-            .executeJwtAuthenticationServiceForDP(hostname)
-            .then((response) => {
-                this.registerSuccessfulLoginForJwt(response.data.username, response.data.accessToken, CHANNEL_DP)
-                console.log("Registered Success");
-                
-                if(sessionStorage.getItem('_target')){
-                    window.location.assign(sessionStorage.getItem('_target'));
-                    sessionStorage.removeItem('_target');
-                }else{
-                    window.location.assign('#/home');
-                }
-                return true;
-            }).catch(() => {
-                window.location.assign('#/');
-                console.log("Registered fail");
-                return false;
-            })
-        }   
-        
-        
-        else if (user === null) {     //------for remote access login
-            return false;
-        }else{
-            return true;
-        }
-    }
-
-    getLoggedInUserName() {
-        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME)
-        if (user === null) return ''
-        return user
-    }
-
-    setupAxiosInterceptors(token) {
-        axios.interceptors.request.use(
-            (config) => {
-                if (this.isUserLoggedIn()) {
-                    config.headers.authorization = token
-                }
-                return config
-            }
-        )
-    }
+  setupAxiosInterceptors(token) {
+    axios.interceptors.request.use((config) => {
+      if (this.isUserLoggedIn()) {
+        config.headers.authorization = token;
+      }
+      return config;
+    });
+  }
 }
 
-export default new AuthenticationService()
+export default new AuthenticationService();
